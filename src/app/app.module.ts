99d810211e3d94a4bb19bec63885ba0232a7f621
manassantra <manas.nshmdgp@gmail.com>
2021-01-31 17:41:30 +0530

    FirstDevelopment

---
+++ src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import { EditComponent } from './components/edit/edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AdunitService } from './adunit.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SlimLoadingBarModule, SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { NavigationCancel,
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes) ,
+    SlimLoadingBarModule ,
+    HttpClientModule ,
+    ReactiveFormsModule
+  ],
+  providers: [AdunitService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  title = 'app';
+  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
+    this._router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this._loadingBar.start();
+    }
+    if (event instanceof NavigationEnd) {
+      this._loadingBar.complete();
+    }
+    if (event instanceof NavigationCancel) {
+      this._loadingBar.stop();
+    }
+    if (event instanceof NavigationError) {
+      this._loadingBar.stop();
+    }
+  }
+}
+
+
+
+
+
---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,34 @@
+// index.component.ts
+import {  Router } from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { AdUnit } from './AdUnit';
+import { AdunitService } from '../../adunit.service';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  adunits: AdUnit[];
+  private _document: any;
+
+  constructor(private adunitservice: AdunitService , private router: Router) { }
+
+  deleteAdUnit(id) {
+    this.adunitservice.deleteAdUnit(id).subscribe(res => {
+      console.log('Deleted');
+      
+    });
+  }
+ 
+  ngOnInit() {
+    this.adunitservice
+      .getAdUnits()
+      .subscribe((data: AdUnit[]) => {
+      this.adunits = data;
+    });
+  }
+}
